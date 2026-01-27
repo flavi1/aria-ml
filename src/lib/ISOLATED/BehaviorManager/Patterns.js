8bfec17a1d5657a7f95ea2b6996398f2a71c8f96
Flavi1 <flavien.guillon@gmail.com>
2026-01-27 18:34:03 +0100

    fix slash for chaining relations

--- src/lib/ISOLATED/BehaviorManager/Patterns.js
+++ src/lib/ISOLATED/BehaviorManager/Patterns.js
@@ -3,7 +3,7 @@ behaviorCore.definePattern('tab', {
     'rel-tablist': '(closest: [role=tablist])',
     'rel-tabpanel': '(root) #{aria-controls}',
     'on-click': `set(tablist@aria-selected, "false")
-        set(tablist.tabpanel@hidden)
+        set(tablist/tabpanel@hidden)
         set(self@aria-selected, "true")
         rm(tabpanel@hidden)`,
     'kb-arrowright': 'focus(next)',
